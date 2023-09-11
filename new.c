d725186d37b3c80d66dd5186799e2acb60882492
Mr. Hasan <hasanshahriar32@gmail.com>
2023-09-11 11:12:32 +0600

    first commit

---
+++ new.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int a, b, c;
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+    printf("%d \n", a);
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+    printf("%d \n", b);
+    c = a + b;
+    printf("The sum of a and b is %d \a", c);
+    return 0;
+}
